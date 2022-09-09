8bd4f2952a866a1f26a5fe17ffeac9847b11d8ed
Kim Hyeong Kyu <hyongkui@nate.com>
2022-09-09 18:53:02 +0900

    161~180

--- Python 300/파이썬 반복문 161~180.py
+++ Python 300/파이썬 반복문 161~180.py
@@ -102,4 +102,15 @@ for i in range(len(my_list) - 1):
     print(abs(my_list[i+1] - my_list[i]))
 
 # 179
+my_list = [100, 200, 400, 800, 1000, 1300]
+
+for i in range(1, len(my_list) - 1):
+    print(abs(my_list[i-1] + my_list[i] + my_list[i+1]) / 3)
+
 # 180
+low_prices  = [100, 200, 400, 800, 1000]
+high_prices = [150, 300, 430, 880, 1000]
+
+volatility = []
+for i in range(len(low_prices)) :
+    volatility.append(high_prices[i] - low_prices[i])
