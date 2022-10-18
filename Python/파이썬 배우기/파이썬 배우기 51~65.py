2cb96105d30eaae96ff31f2304b4d2f49cae52f1
Kim Hyeong Kyu <hyongkui@nate.com>
2022-10-18 23:30:43 +0900

    61~65

--- Python/파이썬 배우기/파이썬 배우기 51~65.py
+++ Python/파이썬 배우기/파이썬 배우기 51~65.py
@@ -99,3 +99,50 @@ for i in range(1, x+1):
 x = int(input())
 for i in range(x, 0, -1):
     print("*"*i)
+
+# 61 2441
+n = int(input())
+
+for i in range(n, 0, -1):
+    print(" " * (n - i) + "*" * i)
+
+# 62 8393
+n = int(input())
+sum = 0
+
+for i in range(1, n + 1):
+    sum += i
+
+print(sum)
+
+# 63 2609
+a, b = map(int, input().split())
+
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
+print(gcd(a, b), a * b // gcd(a, b))
+
+# 64 2748
+N = int(input())
+A = [i for i in range(N + 1)]
+A[1] = 1
+
+for i in range(2, N + 1):
+    A[i] = A[i - 1] + A[i - 2]
+
+print(A[-1])
+
+# 65 5565
+N = int(input())
+A = [i for i in range(N + 1)]
+A[1] = 1
+
+for i in range(2, N + 1):
+    A[i] = A[i - 1] + A[i - 2]
+
+print(A[-1])
