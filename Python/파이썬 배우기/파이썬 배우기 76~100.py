59f8a294289af9b6ce225961010e1ab56b0d96f8
Kim Hyeong Kyu <hyongkui@nate.com>
2022-11-10 23:04:04 +0900

    86~90

--- Python/파이썬 배우기/파이썬 배우기 76~100.py
+++ Python/파이썬 배우기/파이썬 배우기 76~100.py
@@ -72,3 +72,66 @@ print(sum)
 N = int(input())
 for i in range(N):
     print("* "*N if i%2 == 0 else " *"*N)
+
+# 86 10991
+n = int(input())
+for i in range(1, n + 1):
+    print(" " * (n - i) + "* " * (i - 1) + "*")
+
+# 87 10871
+import sys
+N, X = map(int, sys.stdin.readline().split())
+A = list(map(int, sys.stdin.readline().split()))
+
+for n in range(N):
+    if A[n] < X:
+        print(A[n], end = ' ')
+
+# 88 10872
+def factorial(num):
+    if num==0:
+        return 1
+    return num*factorial(num-1)
+
+n=int(input())
+print(factorial(n))
+
+# 89 1978
+input = __import__('sys').stdin.readline
+
+n = int(input())
+ans = 0
+arr = list(map(int, input().split()))
+
+for i in arr:
+    if i < 2:
+        continue
+    flag = True
+    j = 2
+    while j * j <= i:
+        if i % j == 0:
+            flag = False
+            break
+        j += 1
+    if flag:
+        ans += 1
+print(ans)
+
+# 90 2851
+case = []; score = 0
+for _ in range(10):
+    case.append(int(input()))
+for i in range(10):
+    score += case[i]
+    if score == 100:
+        print(score)
+        break
+    elif score > 100 or i == 9:
+        if abs(score - 100) > abs(100 - sum(case[:i])):
+            print(sum(case[:i]))
+            break
+        else:
+            print(score)
+            break
+    else:
+        pass
